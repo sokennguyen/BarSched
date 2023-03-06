3c0d83aaf633de527053bda630aeb40747b07069
Keen <nsongkien@gmail.com>
2023-03-06 15:00:57 +0200

    added timeblocks to Reservation_cal

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,33 +16,83 @@ using System.Data;
 using System.Data.OleDb;
 using System.Windows.Media.Animation;
 using MySqlConnector;
+using System.Collections.ObjectModel;
+using System.Runtime.Remoting.Messaging;
 
 namespace WPF_barber_proto
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+{    public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
-            
+            AddReserveTimeBlock();
         }
+        
+        
+        
         private void ButtonFirst_Click(object sender, RoutedEventArgs e)
         {
-            MySqlConnection connection = new MySqlConnection("Server=2.tcp.eu.ngrok.io;Port=11822;User ID=root;Database=ds_assignment_auction");
-            connection.Open();
-            MySqlCommand command = new MySqlCommand("SELECT auction_id,product_name,auction_start_time,auction_end_time FROM auction",connection);
-            Week_Schedule.ItemsSource=command.ExecuteReader();
-            Week_Schedule.Visibility = Visibility.Visible;
+            AddReserveTimeBlock();
+            MainTitle.Content = "Reservations";
+            CollaspAllView();
+            Reserve_cal.Visibility = Visibility.Visible;
         }
+        
         private void ButtonSecond_Click(object sender, RoutedEventArgs e)
         {
-            Week_Schedule.Visibility= Visibility.Collapsed;
+            //connecting to mysql db requires 3 steps:
+            
+            //create connection + open or close during usages
+            MySqlConnection connection = new MySqlConnection("Server=4.tcp.eu.ngrok.io;Port=17712;User ID=root;Database=ds_assignment_auction");
+            connection.Open();
+
+            //create command
+            MySqlCommand command = new MySqlCommand("SELECT auction_id,product_name,auction_start_time,auction_end_time FROM auction", connection);
+            
+            //execute the command
+            Week_Schedule.ItemsSource = command.ExecuteReader();
+
+            MainTitle.Content = "Add or Remove data";
+            CollaspAllView();
+            Week_Schedule.Visibility = Visibility.Visible;
+        }
+        private void ButtonThird_Click(object sender, RoutedEventArgs e)
+        {
+            MainTitle.Content = "Employee Performances";
+            CollaspAllView();
         }
 
+        private void CollaspAllView()
+        {
+            Week_Schedule.Visibility = Visibility.Collapsed;
+            Reserve_cal.Visibility = Visibility.Collapsed;
+        }
 
+        private void AddReserveTimeBlock()
+        {
+            DateTime datetimeTimeBlock = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 8, 30, 0);
+            int indexAddRow = 1;
+            foreach (RowDefinition row in Reserve_cal.RowDefinitions)
+            {
+                TextBlock outTimeBlock = new TextBlock();
+                string dateTimeBlockString="";
+                if (datetimeTimeBlock.Minute==30)
+                    dateTimeBlockString = datetimeTimeBlock.Hour.ToString() + ":" + datetimeTimeBlock.Minute.ToString();
+                else dateTimeBlockString = datetimeTimeBlock.Hour.ToString() + ":" + datetimeTimeBlock.Minute.ToString() + "0";
+                outTimeBlock.Text = dateTimeBlockString;
+                
+                //set row and add into Reserve_cal grid
+                Grid.SetRow(outTimeBlock, indexAddRow);
+                Reserve_cal.Children.Add(outTimeBlock);
+
+                //increase time and row for next loop
+                if (indexAddRow <23)
+                {
+                    datetimeTimeBlock = datetimeTimeBlock.AddMinutes(30);
+                    indexAddRow++;
+                }
+            }
+        }
 
         //was trying out oledb (Access database), but found a way to use implement mysql 
         private OleDbConnection OpenDB()
