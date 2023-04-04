805e1596edda042cc5c73d0af250e172b2abbabc
Keen <nsongkien@gmail.com>
2023-04-04 14:19:33 +0300

    temp

--- ServiceSubpage.xaml.cs
+++ ServiceSubpage.xaml.cs
@@ -25,99 +25,78 @@ namespace WPF_barber_proto
     /// </summary>
     public partial class ServiceSubpage : UserControl
     {
+        HairdresserProgram HairdresserProgram = new HairdresserProgram();
+        bool isInsertMode = false;
+        bool isBeingEdited = false;
         public ServiceSubpage()
         {
             InitializeComponent();
-            RenderTable();
+            data.ItemsSource = HairdresserProgram.ListService();
         }
-        HairdresserProgram HairdresserProgram = new HairdresserProgram();
-        private void RenderTable()
+
+
+
+        private void Add_Click(object sender, RoutedEventArgs e)
         {
-            int rowIndex = 1;
-            List<Service> serviceList = HairdresserProgram.ListService();
-            foreach (Service service in serviceList)
+            Staff staff;
+            string value = AddBox.Text;
+            List<Staff> AlteredStaffList = new List<Staff>();
+
+            var InsertRecord = MessageBox.Show("Do you want to add " + value + " as a new staff?", "Confirm", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (InsertRecord == MessageBoxResult.Yes)
             {
-                AddRowDef("table");
+                staff = new Staff(null, value);
+                AlteredStaffList = HairdresserProgram.ListStaff();
+                AlteredStaffList.Add(staff);
+                if (HairdresserProgram.SaveStaffChanges(AlteredStaffList))
+                {
+                    data.ItemsSource = HairdresserProgram.ListStaff();
+                    MessageBox.Show("\"" + staff.Name + "\"" + " has being added!", "Inserting Record", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                else
+                {
+                    data.ItemsSource = HairdresserProgram.ListStaff();
+                    MessageBox.Show("Something went wrong", "Inserting Record", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+            AddBox.Text = "";
+        }
 
-                if (rowIndex % 2 == 0) AddRowBgColor("green", "table", rowIndex, 5);
-                else AddRowBgColor("yellow", "table", rowIndex, 5);
 
-                AddTextBlock(service.Id.ToString(), "table", rowIndex, 0);
-                AddTextBlock(service.Name, "table", rowIndex, 1);
-                AddTextBlock(service.Duration.ToString(), "table", rowIndex, 2);
-                AddCheckBox(service.Sink, "table", rowIndex, 3);
-                AddTextBlock(service.PackageName, "table", rowIndex, 4);
-
-                rowIndex++;
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            int countSelected = data.SelectedItems.Count;
+            if (data.SelectedItems.Count > 0)
+            {
+                var Res = MessageBox.Show("Are you sure you want to delete " + data.SelectedItems.Count + " Employees?", "Deleting Records", MessageBoxButton.YesNo, MessageBoxImage.Exclamation);
+                if (Res == MessageBoxResult.Yes)
+                {
+                    foreach (var row in data.SelectedItems)
+                    {
+                        Staff staff = row as Staff;
+                        HairdresserProgram.DeleteStaff(staff);
+                    }
+                    MessageBox.Show(countSelected + " Employees have being deleted!");
+                    data.ItemsSource = HairdresserProgram.ListStaff();
+                }
             }
 
-        }
-        
-        protected void AddCollumnContentControl(string gridName, int colIndex)
-        {
-            var grid = (Grid)this.FindName(gridName);
-            ContentControl contentControl = new ContentControl();
-            contentControl.Name = "textBlockControl";
-            int rowIndex = 1;
-            foreach (RowDefinition row in grid.RowDefinitions)
-            {
-                Grid.SetRow(contentControl, rowIndex);
-                Grid.SetColumnSpan(contentControl, colIndex);
-            }
 
-            contentControl.AddHandler(ButtonBase.ClickEvent, new RoutedEventHandler(contentControl_Click));
-            
-            grid.Children.Add(contentControl);
-        }
-        private void contentControl_Click(object sender, RoutedEventArgs e)
-        {
-            string sourceName = ((FrameworkElement)e.Source).Name;
-            string senderName = ((FrameworkElement)sender).Name;  
         }
 
-        protected void changeTextBlockBgColor(string color,string gridName,int rowIndex, int colIndex)
+
+
+
+        private void dgEmp_AddingNewItem(object sender, AddingNewItemEventArgs e)
         {
-            var grid = (Grid)this.FindName(gridName);
-            Rectangle rectangle = grid.Children.Cast<Rectangle>().First(e => Grid.GetRow(e) == rowIndex && Grid.GetColumn(e) == colIndex);
-            rectangle.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString(color)); 
+            isInsertMode = true;
         }
-        protected void AddRowDef(string gridName)
+
+        private void dgEmp_BeginningEdit(object sender, DataGridBeginningEditEventArgs e)
         {
-            var grid = (Grid)this.FindName(gridName);
-            RowDefinition RowDef = new RowDefinition();
-            RowDef.Height = new GridLength(50);
-            grid.RowDefinitions.Add(RowDef);
+            isBeingEdited = true;
         }
-        protected void AddRowBgColor(string color, string gridName, int rowIndex, int colSpan)
-        {
-            Rectangle bgRec = new Rectangle();
-            bgRec.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString(color));
-            Grid.SetRow(bgRec, rowIndex);
-            Grid.SetColumnSpan(bgRec, colSpan);
-            var grid = (Grid)this.FindName(gridName);
-            grid.Children.Add(bgRec);
-        }
-        protected void AddTextBlock(string inpString, string gridName, int rowIndex, int colIndex)
-        {
-            TextBlock textBlock = new TextBlock();
-            textBlock.Text = inpString;
-            var grid = (Grid)this.FindName(gridName);
-            Grid.SetRow(textBlock, rowIndex);
-            Grid.SetColumn(textBlock, colIndex);
-            textBlock.VerticalAlignment = VerticalAlignment.Center;
-            textBlock.HorizontalAlignment = HorizontalAlignment.Center;
-            grid.Children.Add(textBlock);
-        }
-        protected void AddCheckBox(bool inpBool, string gridName, int rowIndex, int colIndex)
-        {
-            CheckBox checkBox = new CheckBox();
-            checkBox.IsChecked=inpBool;
-            var grid = (Grid)this.FindName(gridName);
-            Grid.SetRow(checkBox, rowIndex);
-            Grid.SetColumn(checkBox, colIndex);
-            checkBox.VerticalAlignment = VerticalAlignment.Center;
-            checkBox.HorizontalAlignment = HorizontalAlignment.Center;
-            grid.Children.Add(checkBox);
-        }
+
+
     }
 }
